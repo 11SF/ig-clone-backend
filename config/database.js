f591d3710e0f2dbb0cfcace52ada250719a9d492
11SF <nsfolknsf@gmail.com>
2021-09-03 04:48:52 +0700

    use .env

--- config/database.js
+++ config/database.js
@@ -1,12 +1,10 @@
+
 const mongoose = require("mongoose");
 require('dotenv').config()
 
-// const username = process.env.DB_USERNAME;
-// const password = process.env.DB_PASSWORD;
-// const dbName = process.env.DB_NAME
-const username = "mentos";
-const password = "dbD9wlpvKVZaZLDa";
-const dbName = "ig-clone"
+const username = process.env.DB_USERNAME;
+const password = process.env.DB_PASSWORD;
+const dbName = process.env.DB_NAME
 
 const mongo_url = `mongodb+srv://${username}:${password}@cluster0.vxyrb.mongodb.net/${dbName}?retryWrites=true&w=majority`;
 mongoose.Promise = global.Promise;
