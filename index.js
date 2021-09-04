6ec22915da09bb0870389617501e0644643c2f19
11SF <nsfolknsf@gmail.com>
2021-09-04 17:54:14 +0700

    added cors

--- index.js
+++ index.js
@@ -5,9 +5,12 @@ const app = express();
 var path = require("path");
 
 require("dotenv").config();
-app.use(cors({
-  origin: ["http://localhost:3000"]
-}))
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000')
+  res.header('Access-Control-Allow-Methods','POST, GET, PUT, PATCH, DELETE, OPTIONS')
+  res.header('Access-Control-Allow-Headers','Content-Type, Option, Authorization,x-access-token')
+  next()
+})
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
